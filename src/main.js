09528f7b7c200972e9e1a0718856044b3a9d4d6e
Leah Miller <leahmiller1969@gmail.com>
2019-08-27 15:44:14 -0700

    added array to App to include Pet. App is being rendered

--- src/main.js
+++ src/main.js
@@ -1,10 +1,22 @@
 
+const Pet =()=> {
+    return React.createElement('div', {}, [
+        React.createElement('h1', {}, 'FatBitch'),
+        React.createElement('h2', {}, 'Skitty'),
+        React.createElement('h2', {}, 'Wild Thing')
+    ]);
+};
+
 const App = () => {
         return React.createElement(
-    "div",
+            "div",
             {id: 'check-it'}, // add `id:'check-it'` to parent element
-            React.createElement('h1', {}, "Adopt Me!") // child element
-    );
+            [
+                React.createElement('h1', {}, "Adopt Me!"), // child element
+                React.createElement(Pet),
+                React.createElement(Pet),
+            ]
+        );
 };
 
 // Render to DOM
