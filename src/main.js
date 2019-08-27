8a8cd0d2dac9c3985d8876dd143daefd37202137
Leah Miller <leahmiller1969@gmail.com>
2019-08-27 16:06:53 -0700

    npm init -y creates new package.json file

--- src/main.js
+++ src/main.js
@@ -8,15 +8,15 @@ const Pet =({name, animal, breed})=> {
 };
 
 const App = () => {
-        return React.createElement(
-            "div",
-            {id: 'check-it'}, // add `id:'check-it'` to parent element
-            [
-                React.createElement('h1', {}, "Adopt Me!"), // child element
-                React.createElement(Pet, {name: "ButtSlut", animal: "cat", breed: "tabby"}),
-                React.createElement(Pet, {name: "The Cat", animal: "kitty", breed: "cat"}),
-            ]
-        );
+    return React.createElement(
+        "div",
+        {id: 'check-it'}, // add `id:'check-it'` to parent element
+        [
+            React.createElement('h1', {}, "Adopt Me!"), // child element
+            React.createElement(Pet, {name: "ButtSlut", animal: "cat", breed: "tabby"}),
+            React.createElement(Pet, {name: "The Cat", animal: "kitty", breed: "cat"}),
+        ]
+    );
 };
 
 // Render to DOM
