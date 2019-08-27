976c274cbc6319fb632cf80a644a2f8f55eec5d3
Leah Miller <leahmiller1969@gmail.com>
2019-08-27 16:23:06 -0700

    opt + shft + F for prettier

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 
 const Pet =({name, animal, breed})=> {
-    return React.createElement('div', {}, [
-        React.createElement('h1', {}, name),
-        React.createElement('h2', {}, animal),
-        React.createElement('h2', {}, breed)
-    ]);
+return React.createElement('div', {}, [
+React.createElement('h1', {}, name),
+React.createElement('h2', {}, animal),
+React.createElement('h2', {}, breed)
+]);
 };
 
 const App = () => {
