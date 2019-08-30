83430d8f9e867a4a2fd2c0130636b79696c8a6b2
Leah Miller <leahmiller1969@gmail.com>
2019-08-30 13:37:09 -0700

    use Search Params component

--- src/main.js
+++ src/main.js
@@ -1,32 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
-  // return React.createElement(
-  //   "div",
-  //   { id: "check-it" }, // add `id:'check-it'` to parent element
-  //   [
-  //     React.createElement("h1", {}, "Adopt Me!"), // child element
-  //     React.createElement(Pet, {
-  //       name: "ButtSlut",
-  //       animal: "cat",
-  //       breed: "tabby"
-  //     }),
-  //     React.createElement(Pet, {
-  //       name: "The Cat",
-  //       animal: "kitty",
-  //       breed: "cat"
-  //     })
-  //   ]
-  // );
-  // code above displays the same as code below but in JSX
   return (
     <div>
       <h1 id="check-it">Adopt Me!</h1>
-      <Pet name="Butt Slut" animal="Cat" breed="Tabby" />
-      <Pet name="The Cat" animal="Kitty" breed="Cat" />
-      <Pet name="Slut Muffin" animal="Skitty" breed="Skat" />
+      <SearchParams />
     </div>
   );
 };
