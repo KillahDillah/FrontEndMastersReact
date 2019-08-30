3bbb0aa3b2e7eece17ee02be4840ce298a39e973
Leah Miller <leahmiller1969@gmail.com>
2019-08-30 14:00:41 -0700

    added hook (location, setLocation) and eventListener (onChange)

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 const SearchParams = () => {
-  const location = "San Francisco, CA";
+  //   const location = "San Francisco, CA";
+  const [location, setLocation] = useState("San Francisco, CA");
 
   return (
     <div className="search-params">
       <form>
         <label htmlFor="location">
           Location
-          <input id="location" value={location} placeholder="Location" />
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={event => setLocation(event.target.value)}
+          />
         </label>
         <button>Submit</button>
       </form>
